89f237e768dc749ee3eec68c879a8562b98876c9
Tom Flídr <tomflidr@gmail.com>
2017-01-27 16:54:49 +0100

    bugfixes

--- src/MvcCore/Ext/Tracy.php
+++ src/MvcCore/Ext/Tracy.php
@@ -11,119 +11,122 @@
  * @license		https://mvccore.github.io/docs/mvccore/4.0.0/LICENCE.md
  */
 
-// Tracy library main class:
-//require_once('Tracy/Debugger.php');
+namespace MvcCore\Ext\Debug {
 
-// MvcCore Tracy extension panel:
-require_once('Tracy/IncludePanel.php');
+	// Tracy library main class:
+	//require_once('Tracy/Debugger.php');
 
-namespace MvcCore\Ext\Debug;
+	// MvcCore Tracy extension panel:
+	require_once('Tracy/IncludePanel.php');
 
-class Tracy extends \MvcCore\Debug {
-	/**
-	 * MvcCore Extension - Debug Tracy - version:
-	 * Comparation by PHP function version_compare();
-	 * @see http://php.net/manual/en/function.version-compare.php
-	 */
-	const VERSION = '4.0.0';
-	/**
-	 * Auto initialize all panel classes if exists in registry bellow.
-	 * @var bool
-	 */
-	public static $autoInitPanels = TRUE;
-	/**
-	 * Extended Tracy panels registry for automatic panel initialization.
-	 * If panel class exists, it is automaticly created and registred into Tracy bar.
-	 * @var string[]
-	 */
-	public static $ExtendedPanels = array(
-		'MvcCorePanel',
-		'SessionPanel',
-		'RoutingPanel',
-		'AuthPanel',
-		// 'IncludePanel', // added every time strictly, not in foreach
-	);
-	/**
-	 * Add editor key for every Tracy editor link 
-	 * to open your files in specific editor.
-	 * @var string
-	 */
-	public static $Editor = '';
-	/**
-	 * Initialize debuging and loging.
-	 * @param boolean $debugMode TRUE for development, FALSE for production.
-	 * @return void
-	 */
-	public static function Init () {
-		if (!is_null(static::$development)) return;
-		parent::Init();
-		\Tracy\Debugger::$maxDepth = 4;
-		if (isset(\Tracy\Debugger::$maxLen)) { // backwards compatibility
-			\Tracy\Debugger::$maxLen = 5000;
-		} else {
-			\Tracy\Debugger::$maxLength = 5000;
-		}
-		if (static::$Editor) \Tracy\Debugger::$editor .= '&editor=' . static::$Editor;
-		$tracyBar = \Tracy\Debugger::getBar();
-		foreach (static::$ExtendedPanels as $panelName) {
-			$panelName = '\MvcCore\Ext\Debug\Tracy_' . $panelName;
-			if (class_exists($panelName)) {
-				$panel = new $panelName();
-				$tracyBar->addPanel($panel, $panel->getId());
-			}
-		}
-		$includePanel = new \MvcCore\Ext\Debug\Tracy\IncludePanel();
-		$tracyBar->addPanel($includePanel, $includePanel->getId());
-		\Tracy\Debugger::enable(!static::$development, static::$LogDirectory, static::$EmailRecepient);
-	}
-
-	/**
-	 * Initialize debuging and loging handlers.
-	 * @return void
-	 */
-	protected static function initHandlers () {
-		foreach (static::$handlers as $key => $value) {
-			static::$handlers[$key] = array('\Tracy\Debugger', $key);
-		}
-		static::$handlers = (object) static::$handlers;
-	}
-
-	/**
-	 * If log directory doesn't exist, create new directory - relative from app root.
-	 * @param string $logDirectory relative path from app root
-	 * @return void
-	 */
-	protected static function initGlobalShortHands () {
+	class Tracy extends \MvcCore\Debug {
+		/**
+		 * MvcCore Extension - Debug Tracy - version:
+		 * Comparation by PHP function version_compare();
+		 * @see http://php.net/manual/en/function.version-compare.php
+		 */
+		const VERSION = '4.0.0';
+		/**
+		 * Auto initialize all panel classes if exists in registry bellow.
+		 * @var bool
+		 */
+		public static $autoInitPanels = TRUE;
+		/**
+		 * Extended Tracy panels registry for automatic panel initialization.
+		 * If panel class exists, it is automaticly created and registred into Tracy bar.
+		 * @var string[]
+		 */
+		public static $ExtendedPanels = array(
+			'MvcCorePanel',
+			'SessionPanel',
+			'RoutingPanel',
+			'AuthPanel',
+			// 'IncludePanel', // added every time strictly, not in foreach
+		);
+		/**
+		 * Add editor key for every Tracy editor link 
+		 * to open your files in specific editor.
+		 * @var string
+		 */
+		public static $Editor = '';
 
 		/**
-		 * Dump a variable in Tracy Debug Bar.
-		 * @param	mixed	$value		variable to dump
-		 * @param	string	$title		optional title
-		 * @param	array	$options	dumper options
-		 * @return	mixed				variable itself
-		*/
+		 * Initialize global development shorthands.
+		 * @param string $logDirectory relative path from app root
+		 * @var callable
+		 */
+		public static $InitGlobalShortHands = array();
+
+		/**
+		 * Initialize debuging and loging.
+		 * @param boolean $debugMode TRUE for development, FALSE for production.
+		 * @return void
+		 */
+		public static function Init () {
+			if (!is_null(static::$development)) return;
+			parent::Init();
+			\Tracy\Debugger::$maxDepth = 4;
+			if (isset(\Tracy\Debugger::$maxLen)) { // backwards compatibility
+				\Tracy\Debugger::$maxLen = 5000;
+			} else {
+				\Tracy\Debugger::$maxLength = 5000;
+			}
+			if (static::$Editor) \Tracy\Debugger::$editor .= '&editor=' . static::$Editor;
+			$tracyBar = \Tracy\Debugger::getBar();
+			foreach (static::$ExtendedPanels as $panelName) {
+				$panelName = '\MvcCore\Ext\Debug\Tracy\\' . $panelName;
+				if (class_exists($panelName)) {
+					$panel = new $panelName();
+					$tracyBar->addPanel($panel, $panel->getId());
+				}
+			}
+			$includePanel = new \MvcCore\Ext\Debug\Tracy\IncludePanel();
+			$tracyBar->addPanel($includePanel, $includePanel->getId());
+			\Tracy\Debugger::enable(!static::$development, static::$LogDirectory, static::$EmailRecepient);
+		}
+
+		/**
+		 * Initialize debuging and loging handlers.
+		 * @return void
+		 */
+		protected static function initHandlers () {
+			foreach (static::$handlers as $key => $value) {
+				static::$handlers[$key] = array('\Tracy\Debugger', $key);
+			}
+			static::$handlers = (object) static::$handlers;
+		}
+	}
+}
+
+namespace {
+	\MvcCore\Ext\Debug\Tracy::$InitGlobalShortHands = function () {
+		/**
+			* Dump a variable in Tracy Debug Bar.
+			* @param	mixed	$value		variable to dump
+			* @param	string	$title		optional title
+			* @param	array	$options	dumper options
+			* @return	mixed				variable itself
+			*/
 		function x ($value, $title = NULL, $options = array()) {
 			return \Tracy\Debugger::barDump($value, $title, $options);
-		};
-
+		}
 		/**
-		 * Dumps variables about a variable in Tracy Debug Bar.
-		 * @param	...mixed	variables to dump
-		 * @return	void
-		*/
+			* Dumps variables about a variable in Tracy Debug Bar.
+			* @param	...mixed	variables to dump
+			* @return	void
+			*/
 		function xx () {
 			$args = func_get_args();
 			foreach ($args as $arg) \Tracy\Debugger::barDump($arg);
-		};
-
+		}
 		/**
-		 * Dump a variable in Tracy Debug Bar and die. If no variable, throw stop exception.
-		 * @param	mixed		variable to dump
-		 * @param	string		optional title
-		 * @param	array		dumper options
-		 * @throws	\Exception
-		 * @return	void
-		 */
+			* Dump a variable in Tracy Debug Bar and die. If no variable, throw stop exception.
+			* @param	mixed		variable to dump
+			* @param	string		optional title
+			* @param	array		dumper options
+			* @throws	\Exception
+			* @return	void
+			*/
 		function xxx ($var = NULL) {
 			$args = func_get_args();
 			if (count($args) === 0) {
@@ -134,6 +137,6 @@ class Tracy extends \MvcCore\Debug {
 			}
 			echo ob_get_clean();
 			die();
-		};
-	}
+		}
+	};
 }
