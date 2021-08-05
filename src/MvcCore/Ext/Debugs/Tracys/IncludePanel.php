54cedb02905d6375de9ee818cda387fb21aef125
Tom Flídr <tomflidr@gmail.com>
2021-08-05 10:35:24 +0200

    Include panel improvements.

--- src/MvcCore/Ext/Debugs/Tracys/IncludePanel.php
+++ src/MvcCore/Ext/Debugs/Tracys/IncludePanel.php
@@ -24,19 +24,19 @@ class IncludePanel implements \Tracy\IBarPanel {
 	 * All used PHP files list by current request.
 	 * @var \string[]
 	 */
-	protected static $files = [];
+	protected $files = [];
 
 	/**
 	 * Used PHP files count except files used by `Tracy` extension.
 	 * @var mixed
 	 */
-	protected static $appFilesCount = 0;
+	protected $appFilesCount = 0;
 
 	/**
 	 * All used PHP files count.
 	 * @var int
 	 */
-	protected static $allFilesCount = 0;
+	protected $allFilesCount = 0;
 
 	/**
 	 * Get unique `Tracy` debug bar panel id.
@@ -53,7 +53,7 @@ class IncludePanel implements \Tracy\IBarPanel {
 	public function getTab() {
 		static::completeFilesCountsAndEditorLinks();
 		return '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAADtUExURQAAAANAWAULDjxIVExOUQsKCA4nMANOaxYyPQJCWg0ZHgJQbQYMDyM8UzVrnmhXHl1eYm5uaHx8gQolLVJfaz9PXm2TtrOPIH1wMV5TJ29kLQI8UtWtJkhKK3FmKtOvLAQ1SARQbTMqHIyMjISEicLCz6zS9GdyeN/f6vf3+Vai6qOjymtrrb3Z83y37o3A8Obt9TaI03dxWdDY3ZmannJyr1BQoXeAhMPH3dTU37291rXG183j+Nbo+WWQuWqk2mCo6om+72Ko61JRQnN+hUczHJGepiBNd2ROJzhaeJijpF1dpkdHm5iYxe7u8lzeC4UAAAAkdFJOUwACQpD4JkALx2Z+RU/8/dr9+f28q6H982I0Snb1y/Pvn1zx+/LnWboAAAC0SURBVBjTbczVFoJAFIXhoUtQ7M4RKUUBBbs73v9xZAD1xu/u/GuvAwCC4QSBgx8mO18oKx77hlx/uFQGIv8ZUXmlr1gb8UAmopBYb63BbnQ8uXK0oSRd10d72fG8DINCqX6Wrhfb0TSNRaHcSGba9s1XVT8MxWqSI1NDYzKZPV9BoAq1FgOhen+YhqmiBS1goAOn5nhmjKfhjwBMdyOfkHZ7AZJlm3EgIZKqMPENaC4kgD/e1akVKEqC52oAAAAASUVORK5CYII="/>'
-			. self::$appFilesCount;
+			. $this->appFilesCount;
 	}
 
 	/**
@@ -62,7 +62,7 @@ class IncludePanel implements \Tracy\IBarPanel {
 	 */
 	public function getPanel() {
 		self::completeFilesCountsAndEditorLinks();
-		$usedFilesListCode = join("", static::$files);
+		$usedFilesListCode = join("", $this->files);
 		return '<style type="text/css">'
 				.'#tracy-include-panel{overflow:hidden;}'
 				.'.tracy-mode-float #tracy-include-panel{overflow:visible;}'
@@ -71,7 +71,7 @@ class IncludePanel implements \Tracy\IBarPanel {
 				.'#tracy-include-panel .content .tracy{color:#7a91a9;background:#eee;}'
 			.'</style>'
 			.'<div id="tracy-include-panel">'
-				.'<h1>Included app files: '.static::$appFilesCount.' (all: '.static::$allFilesCount.')</h1>'
+				.'<h1>Included app files: '.$this->appFilesCount.' (all: '.$this->allFilesCount.')</h1>'
 				.'<div class="content"><code>'
 					.$usedFilesListCode
 				.'<code></div>'
@@ -82,25 +82,54 @@ class IncludePanel implements \Tracy\IBarPanel {
 	 * Complete final used PHP files list as list of HTML codes with editor links.
 	 * @return void
 	 */
-	protected static function completeFilesCountsAndEditorLinks () {
-		if (!static::$files) {
+	protected function completeFilesCountsAndEditorLinks () {
+		if (!$this->files) {
 			$rawList = get_included_files();
 			$list = [];
-			$docRoot = \MvcCore\Application::GetInstance()->GetRequest()->GetAppRoot();
-			$docRootLength = mb_strlen($docRoot);
+			$appRoot = \MvcCore\Application::GetInstance()->GetRequest()->GetAppRoot();
+			$appRootLen = mb_strlen($appRoot);
 			$tracyFileDetectionSubstr = '/tracy';
 			foreach ($rawList as $file) {
 				$file = str_replace('\\', '/', $file);
-				$text = (mb_strpos($file, $docRoot) === 0)
-					? mb_substr($file, $docRootLength)
-					: $file;
+				$text = $this->getVisibleFilePath($file, $appRoot, $appRootLen);
 				$tracyFile = mb_stripos($text, $tracyFileDetectionSubstr) !== FALSE;
-				if (!$tracyFile) static::$appFilesCount += 1;
-				static::$allFilesCount += 1;
+				if (!$tracyFile) $this->appFilesCount += 1;
+				$this->allFilesCount += 1;
 				$href = \Tracy\Helpers::editorUri($file, 1);
 				$list[] = '<a '.($tracyFile ? 'class="tracy" ':'').'href="'.$href.'">'.$text.'</a><br />';
 			}
-			static::$files = & $list;
+			$this->files = & $list;
 		}
+	}
+
+	/**
+	 * Return file path to render in link text.
+	 * If there is found application root in path, 
+	 * return only path after it, if not, return 
+	 * three dots, two parent folders and filename.
+	 * @param  string $file 
+	 * @param  string $appRoot 
+	 * @param  int    $appRootLen 
+	 * @return string
+	 */
+	protected function getVisibleFilePath ($file, $appRoot, $appRootLen) {
+		$result = $file;
+		if (mb_strpos($file, $appRoot) === 0) {
+			$result = mb_substr($file, $appRootLen);
+		} else {
+			$i = 0;
+			$pos = mb_strlen($file) + 1;
+			while ($i < 5) {
+				$pos = mb_strrpos(mb_substr($file, 0, $pos - 1), '/');
+				if ($pos === FALSE) break; 
+				$i++;
+			}
+			if ($pos === FALSE) {
+				$result = $file;
+			} else {
+				$result = '&hellip;'.mb_substr($file, $pos);
+			}
+		}
+		return $result;
 	}
 }
